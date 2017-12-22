62994a22b2207afd33bfba590d335d663645bd75
TMA\nbxtruong <omega220@gmail.com>
2017-12-22 08:34:34 +0700

    Apply OTA Update from S3

--- ConfigPortalAndAWSIOT/ConfigPortalAndAWSIOT.ino
+++ ConfigPortalAndAWSIOT/ConfigPortalAndAWSIOT.ino
@@ -21,6 +21,9 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
+//Library for OTA
+#include <Update.h>
+
 //Connect to DHT by using PIN 14
 #define DHTPIN 14
 
@@ -35,6 +38,7 @@ AWS_IOT hornbill;
 #define TRIGGER_PIN 0
 #define LED_PIN 2
 WiFiManager wifiManager;
+WiFiClient wClient;
 
 char HOST_ADDRESS[] = "a2oxjrmrtmst02.iot.us-west-2.amazonaws.com";
 char CLIENT_ID[] = "TruongESP32";
@@ -46,6 +50,16 @@ char payload[512];
 char rcvdPayload[512];
 uint32_t delayMS;
 
+// Variables to validate
+// response from S3
+int contentLength = 0;
+bool isValidContentType = false;
+
+// S3 Bucket Config
+String host = "esp32dat.s3-ap-southeast-1.amazonaws.com"; // Host => bucket-name.s3.region.amazonaws.com
+int port = 80; // Non https. For HTTPS 443. As of today, HTTPS doesn't work.
+String bin = "/ConfigPortalAndAWSIOT.ino.doitESP32devkitV1.bin"; // bin file name with a slash in front.
+
 // List of callback function
 void mySubCallBackHandler (char *topicName, int payloadLen, char *payLoad) {
   strncpy(rcvdPayload, payLoad, payloadLen);
@@ -53,11 +67,140 @@ void mySubCallBackHandler (char *topicName, int payloadLen, char *payLoad) {
   msgReceived = 1;
 }
 
+// Utility to extract header value from headers
+String getHeaderValue(String header, String headerName) {
+  return header.substring(strlen(headerName.c_str()));
+}
+
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("Entered config mode");
   Serial.println(WiFi.softAPIP());
 }
 // End list of callback function
+
+// OTA Logic
+void execOTA() {
+  Serial.println("Connecting to: " + String(host));
+  // Connect to S3
+  if (wClient.connect(host.c_str(), port)) {
+    // Connection Succeed.
+    // Fecthing the bin
+    Serial.println("Fetching Bin: " + String(bin));
+
+    // Get the contents of the bin file
+    wClient.print(String("GET ") + bin + " HTTP/1.1\r\n" +
+                  "Host: " + host + "\r\n" +
+                  "Cache-Control: no-cache\r\n" +
+                  "Connection: close\r\n\r\n");
+
+    // Check what is being sent
+    // Serial.print(String("GET ") + bin + " HTTP/1.1\r\n" +
+    //              "Host: " + host + "\r\n" +
+    //              "Cache-Control: no-cache\r\n" +
+    //              "Connection: close\r\n\r\n");
+
+    unsigned long timeout = millis();
+    while (wClient.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println("Client Timeout !");
+        wClient.stop();
+        return;
+      }
+    }
+
+    while (wClient.available()) {
+      // read line till /n
+      String line = wClient.readStringUntil('\n');
+      // remove space, to check if the line is end of headers
+      line.trim();
+      // Serial.println(String(line));
+
+      // if the the line is empty,
+      // this is end of headers
+      // break the while and feed the
+      // remaining `client` to the
+      // Update.writeStream();
+      if (!line.length()) {
+        //headers ended
+        break; // and get the OTA started
+      }
+
+      // Check if the HTTP Response is 200
+      // else break and Exit Update
+      if (line.startsWith("HTTP/1.1")) {
+        if (line.indexOf("200") < 0) {
+          Serial.println("Got a non 200 status code from server. Exiting OTA Update.");
+          break;
+        }
+      }
+
+      // extract headers here
+      // Start with content length
+      if (line.startsWith("Content-Length: ")) {
+        contentLength = atoi((getHeaderValue(line, "Content-Length: ")).c_str());
+        Serial.println("Got " + String(contentLength) + " bytes from server");
+      }
+
+      // Next, the content type
+      if (line.startsWith("Content-Type: binary/octet-stream")) {
+        isValidContentType = true;
+      }
+    }
+  } else {
+    // Connect to S3 failed
+    // May be try?
+    // Probably a choppy network?
+    Serial.println("Connection to " + String(host) + " failed. Please check your setup");
+    // retry??
+    // execOTA();
+  }
+
+  // Check what is the contentLength and if content type is `application/octet-stream`
+  Serial.println("contentLength : " + String(contentLength) + ", isValidContentType : " + String(isValidContentType));
+
+  // check contentLength and content type
+  if (contentLength && isValidContentType) {
+    // Check if there is enough to OTA Update
+    bool canBegin = Update.begin(contentLength);
+
+    // If yes, begin
+    if (canBegin) {
+      Serial.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quite for a while.. Patience!");
+      // No activity would appear on the Serial monitor
+      // So be patient. This may take 2 - 5mins to complete
+      size_t written = Update.writeStream(wClient);
+
+      if (written == contentLength) {
+        Serial.println("Written : " + String(written) + " successfully");
+      } else {
+        Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?" );
+        // retry??
+        // execOTA();
+      }
+
+      if (Update.end()) {
+        Serial.println("OTA done!");
+        if (Update.isFinished()) {
+          Serial.println("Update successfully completed. Rebooting.");
+          ESP.restart();
+        } else {
+          Serial.println("Update not finished? Something went wrong!");
+        }
+      } else {
+        Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+      }
+    } else {
+      // not enough space to begin OTA
+      // Understand the partitions and
+      // space availability
+      Serial.println("Not enough space to begin OTA");
+      wClient.flush();
+    }
+  } else {
+    Serial.println("There was no content in the response");
+    wClient.flush();
+  }
+}
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
@@ -181,8 +324,12 @@ void loop() {
     }
   }
 
-  if (tick >= 5) { // publish to topic every 5seconds
-    tick = 0;
+  if (tick % 2 == 0) { // publish to topic every 5seconds
+    if (tick >= 3600) {
+      // Execute OTA Update for every 1 hour and reset "tick" variable
+      tick = 0;
+      execOTA();
+    }
     sprintf(payload, "{\"state\": { \"desired\": { \"welcome\": %d, \"temperature\": %f, \"humidity\": %f }}}", msgCount++, temperature, humidity );
     if (hornbill.publish(TOPIC_NAME, payload) == 0) {
       Serial.print("Publish Message:");
@@ -192,6 +339,7 @@ void loop() {
       Serial.println("Publish failed");
     }
   }
+
   vTaskDelay(1000 / portTICK_RATE_MS);
   tick++;
 }
